13f8c9abd9065098edd7eb2b0fe11048bc06a816
AiaRup <aiale88@gmail.com>
2018-04-20 12:30:06 +0300

    order everything in folders, add server.js file and did npm install

--- public/script.js
+++ public/script.js
@@ -5,17 +5,17 @@ var updateCart = function () {
   // TODO: Write this function. In this function we render the page.
   // Meaning we make sure that all our cart items are displayed in the browser.
   // Remember to empty the "cart div" before you re-add all the item elements.
-}
+};
 
 
 var addItem = function (item) {
-  // TODO: Write this function. Remember this function has nothing to do with display. 
+  // TODO: Write this function. Remember this function has nothing to do with display.
   // It simply is for adding an item to the cart array, no HTML involved - honest ;-)
-}
+};
 
 var clearCart = function () {
   // TODO: Write a function that clears the cart ;-)
-}
+};
 
 $('.view-cart').on('click', function () {
   // TODO: hide/show the shopping cart!
