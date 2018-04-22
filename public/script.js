842a1a708d35c90a173b70f4398980411ef5b1f7
AiaRup <aiale88@gmail.com>
2018-04-22 20:30:56 +0300

    add modular code

--- public/script.js
+++ public/script.js
@@ -1,38 +1,50 @@
 // declare variables
 var shoppingCart = $('.shopping-cart');
 var shoppingList = shoppingCart.find('.cart-list');
-// an array with all of our cart items
-var cart = [];
 
-var updateCart = function() {
-  //Write this function. In this function we render the page.
-  // Meaning we make sure that all our cart items are displayed in the browser.
-  // Remember to empty the "cart div" before you re-add all the item elements.
-  shoppingList.empty();
-  var output = '';
-  var totalPrice = 0;
-  for (var i = 0; i< cart.length; i +=1) {
-    output += '<p>' + cart[i].name + ' - $' + cart[i].price + '</p>';
-    totalPrice += cart[i].price;
-  }
-  shoppingList.append(output);
-  shoppingCart.find('.total').text(totalPrice);
+var shoppingApp = function() {
+  // an array with all of our cart items
+  var cart = [];
+
+  var updateCart = function() {
+    //Write this function. In this function we render the page.
+    // Meaning we make sure that all our cart items are displayed in the browser.
+    // Remember to empty the "cart div" before you re-add all the item elements.
+    shoppingList.empty();
+    var output = '';
+    var totalPrice = 0;
+    for (var i = 0; i < cart.length; i += 1) {
+      output += '<p>' + cart[i].name + ' - $' + cart[i].price + '</p>';
+      totalPrice += cart[i].price;
+    }
+    shoppingList.append(output);
+    shoppingCart.find('.total').text(totalPrice);
+  };
+
+  var addItem = function(item) {
+    //Write this function. Remember this function has nothing to do with display.
+    // It simply is for adding an item to the cart array, no HTML involved - honest ;-)
+    cart.push(item);
+  };
+
+  var clearCart = function() {
+    //Write a function that clears the cart ;-)
+    cart = [];
+    shoppingList.empty();
+  };
+  return {
+    updateCart: updateCart,
+    addItem: addItem,
+    clearCart: clearCart
+  };
 };
 
+var app = shoppingApp();
 
-var addItem = function(item) {
-  //Write this function. Remember this function has nothing to do with display.
-  // It simply is for adding an item to the cart array, no HTML involved - honest ;-)
-  cart.push(item);
-};
+// update the cart as soon as the page loads!
+app.updateCart();
 
-var clearCart = function() {
-  //Write a function that clears the cart ;-)
-  cart = [];
-  shoppingList.empty();
-
-};
-
+/******** EVENT LISTENERS **********/
 $('.view-cart').on('click', function() {
   //hide/show the shopping cart!
   shoppingCart.toggleClass('show');
@@ -45,15 +57,12 @@ $('.add-to-cart').on('click', function() {
   item.name = itemHTML.data().name;
   item.price = itemHTML.data().price;
 
-  addItem(item);
-  updateCart();
+  app.addItem(item);
+  app.updateCart();
 });
 
 $('.clear-cart').on('click', function() {
-  clearCart();
+  app.clearCart();
   shoppingCart.find('.total').text('0');
-  updateCart();
+  app.updateCart();
 });
-
-// update the cart as soon as the page loads!
-updateCart();
